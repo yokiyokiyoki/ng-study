c2e2edd49125377f071832a1d507995779e357c8
youjie <youjie@hudongpai.com>
2018-04-17 10:53:32 +0800

    引入formMoudule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-
+import { FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule],

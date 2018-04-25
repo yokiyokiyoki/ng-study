e1f92f1bec00c911c45b2abd6587845662119228
youjie <youjie@hudongpai.com>
2018-04-25 15:03:33 +0800

    注入 HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { Hero } from "../hero";
 
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 @Component({
   selector: "app-heroes",
   templateUrl: "./heroes.component.html",
@@ -11,11 +11,11 @@ import { HEROES } from "../mock-heroes";
 //实现了OnInit接口的类
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {}
 }

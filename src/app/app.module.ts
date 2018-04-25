f98f518d084f7f04dc01193a010499b0202a7bfd
youjie <youjie@hudongpai.com>
2018-04-25 15:00:30 +0800

    准备依赖注入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./hero.service";
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

61fb374b31f578e6dfa43260ebf1774a1ee3d5a7
youjie <youjie@hudongpai.com>
2018-04-25 15:56:31 +0800

    message的服务

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { HeroService } from "./hero.service";
 import { MessageComponent } from './message/message.component';
+import { MessageService } from './message.service';
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessageComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [HeroService],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

2e9c13b3dc406d9353976ca5ab583b04bff5c4ea
youjie <youjie@hudongpai.com>
2018-04-25 16:31:35 +0800

    绑定到 MessageService

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { MessageService } from "../message.service";
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  selector: "app-message",
+  templateUrl: "./message.component.html",
+  styleUrls: ["./message.component.css"]
 })
 export class MessageComponent implements OnInit {
+  //必须是公共属性，模板中会用到
+  constructor(public messageService: MessageService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

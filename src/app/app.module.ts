aa66d3a2abfd92e92dd169007a918d3d808156f3
youjie <youjie@hudongpai.com>
2018-05-10 18:55:35 +0800

    启用http服务

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { HeroService } from "./hero.service";
-import { MessageComponent } from './message/message.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessageComponent } from "./message/message.component";
+import { MessageService } from "./message.service";
+import { AppRoutingModule } from ".//app-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessageComponent, DashboardComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent,
+    DashboardComponent
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClient],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })

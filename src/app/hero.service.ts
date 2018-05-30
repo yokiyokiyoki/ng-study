b3ed1786b33bdfd72daf83b3bf3509cd16d515f3
youjie <youjie@hudongpai.com>
2018-05-30 11:51:45 +0800

    错误消息处理

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from "./mock-heroes";
 import { Observable } from "rxjs";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 @Injectable()
 export class HeroService {
   constructor(
@@ -15,11 +16,25 @@ export class HeroService {
   }
   private heroesUrl = "api/heroes";
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add("HeroService:fetched heroes");
-    return Observable.of(HEROES);
+    this.log("HeroService:fetched heroes");
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError("getHeroes", [])));
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return Observable.of(HEROES.find(hero => hero.id === id));
+  }
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return Observable.of(result as T);
+    };
   }
 }

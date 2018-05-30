69d480f542d27a024ed5cc7b1409896a237351b3
youjie <youjie@hudongpai.com>
2018-05-30 16:07:41 +0800

    阶段性提交

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./in-memory-data.service";
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroSearchComponent } from "./hero-search/hero-search.component";
 @NgModule({
   declarations: [
     AppComponent,
